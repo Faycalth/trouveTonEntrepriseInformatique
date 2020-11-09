4c77d3c03b54c0e5665660903fa8b509eec73073
Faycalth <faycalthamri38@gmail.com>
2020-11-09 01:46:59 +0100

    amelioration du script

--- script.py
+++ script.py
@@ -1,37 +1,31 @@
 #https://docs.python.org/fr/3/library/csv.html
-import csv
-nom_fichier_src = "liste_entreprise.csv"
-nom_fichier_csv = "liste_entreprise_triees.csv"
-liste_commune = ["LYON", "PARIS 8", "VILLEURBANNE", "CRETEIL"]
-fieldnames = ['Nom', 'Ville', 'Code Postal', 'Categorie']
+import csv, time
 
-#
+nom_fichier_src = 'liste_entreprise.csv'
+nom_fichier_csv = 'liste_entreprise_trouvées.csv'
+liste_commune = ['LYON 1ER', 'LYON 2EME', 'LYON 6EME', 'LYON 7EME', 'LYON 8EME', 'VILLEURBANNE']
+fieldnames = ['Categorie', 'Nom', 'Code Postal', 'Ville', 'Activite principale']
+#dict_act = dict()
+dict_act = {'61.10Z': 'Telecommunications filaires', '61.20Z': 'Telecommunications sans fil', '61.30Z': 'Télécommunications par satellite','61.90Z': 'Autres activités de télécommunication','62.01Z': 'Programmation informatique','62.02A': 'Conseil en systèmes et logiciels informatiques','62.02B': 'Tierce maintenance de systèmes et d applications informatiques','62.03Z': 'Gestion d installations informatiques','62.09Z': 'Autres activités informatiques','63.11Z': 'Traitement de données, hébergement et activités connexes','63.12Z': 'Portails Internet'}
+count = 0
+
 #Ouverture du fichier
-with open(nom_fichier_src, "r") as fichier_src, open(nom_fichier_csv, "w") as fichier_csv:
+with open(nom_fichier_src, "r") as fichier_src, open(nom_fichier_csv, "w", newline="") as fichier_csv:
     writer = csv.DictWriter(fichier_csv, fieldnames=fieldnames, restval="")
     writer.writeheader()
     
     reader = csv.DictReader(fichier_src)
     for row in reader:
-        if(row['libelleCommuneEtablissement'] in liste_commune):
-            writer.writerow({
-                'Nom':row['denominationUniteLegale'], 
-                'Ville':row['libelleCommuneEtablissement'], 
-                'Code Postal':row['codePostalEtablissement'], 
-                'Categorie':row['categorieEntreprise']
-            })
-
-"""
+        if(row['libelleCommuneEtablissement'] in liste_commune and row['categorieJuridiqueUniteLegale']!="1000"):
+            if(row['activitePrincipaleUniteLegale'] in dict_act):
+                writer.writerow({
+                    'Categorie':row['categorieEntreprise'],
+                    'Nom':row['denominationUniteLegale']+" "+row['sigleUniteLegale'],
+                    'Ville':row['libelleCommuneEtablissement'], 
+                    'Code Postal':row['codePostalEtablissement'],
+                    'Activite principale': dict_act[row['activitePrincipaleUniteLegale']]
+                })
+                count = count + 1
     
-    
-    #Parcours du fichier
-    lignes = fichier_src.read()
-    i=0
-
-    while i<10:
-        print(lignes)
-        i = i +1
- """       
-    
-    
-print("Programme terminé")
+print("Nombre d'entreprises trouvées : ", count)
+print("Temps d'éxecution :", time.perf_counter(), "secondes.")
